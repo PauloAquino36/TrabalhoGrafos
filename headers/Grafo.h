e83c7479a072204cd3e29135758493a0fe6e5239
Lucas Rocha <Lucas.Rocha@thomsonreuters.com>
2024-12-31 01:21:56 -0300

    Ajustes nas declaracoes da classe Grafo

--- headers/Grafo.h
+++ headers/Grafo.h
@@ -14,24 +14,25 @@ protected:
     bool ponderadoArestas;
 
 public:
-     bool eh_bipartido();                   //  Lucas
-     int n_conexo();                        //  Lucas
-     int get_grau();                        //  Leandro // Ja esta Feita :)
-     int get_ordem();                       //  Leandro // Ja esta Feita :)
-     bool eh_direcionado();                 //  Paulo // Ja esta Feita :)
-     bool vertice_ponderado();              //  Paulo // Ja esta Feita :)
-     bool aresta_ponderada();               //  Paulo // Ja esta Feita :)
-     bool eh_completo();                    //  Leticia
-     bool eh_arvore();                      //  Leticia
-     bool possui_articulacao();             //  Favio
-     bool possui_ponte();                   //  Favio
+    bool eh_bipartido();                   //  Lucas
+    int n_conexo();                        //  Lucas
+    int get_grau();                        //  Leandro // Ja esta Feita :)
+    int get_ordem();                       //  Leandro // Ja esta Feita :)
+    bool eh_direcionado();                 //  Paulo // Ja esta Feita :)
+    bool vertice_ponderado();              //  Paulo // Ja esta Feita :)
+    bool aresta_ponderada();               //  Paulo // Ja esta Feita :)
+    bool eh_completo();                    //  Leticia
+    bool eh_arvore();                      //  Leticia
+    bool possui_articulacao();             //  Favio
+    bool possui_ponte();                   //  Favio
+    static Grafo* gera_grafo(const std::string& nomeArquivo); //  Lucas
 
-     Grafo(int numVertices, bool direcionado, bool ponderadoVertices, bool ponderadoArestas, int grau);
-     ~Grafo();
+    Grafo(int numVertices, bool direcionado, bool ponderadoVertices, bool ponderadoArestas, int grau);
+    ~Grafo();
 
-     //funçoes auxiliares
-     Vertice* getVertices();
-     void adicionarAresta(int origem, int destino, int peso);
+    //funçoes auxiliares
+    Vertice* getVertices();
+    void adicionarAresta(int origem, int destino, int peso);
 };
 
 #endif
