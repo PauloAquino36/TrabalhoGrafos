1941837e245d676eb3ab34bdab09647a83e714d8
Lucas Rocha <lucas.lgr.01@gmail.com>
2025-02-09 19:36:36 -0300

    ajustando n_conexo

--- src/Grafo.cpp
+++ src/Grafo.cpp
@@ -23,20 +23,19 @@ int Grafo::n_conexo()
 {
     bool* visitado = new bool[numVertices];
 
-    for (int i = 0; i < numVertices; i++) {
+    for (int i = 1; i <= numVertices; i++) {
         visitado[i] = false;
     }
 
     int numComponentes = 0;
 
-    for (int i = 0; i < numVertices; i++) {
+    for (int i = 1; i <= numVertices; i++) {
         if (!visitado[i]) {
             dfs(i, visitado);
             numComponentes++;
         }
     }
 
-    cout << "Numero de componentes conexas: " << numComponentes << endl;
     delete[] visitado; // Agora não vai quebrar
     return numComponentes;
 }
