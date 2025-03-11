6f8a73581abf55118bc10fee4a292b05babf0eba
Lucas Rocha <lucas.lgr.01@gmail.com>
2025-03-10 23:50:06 -0300

    script para gerar grafo aleatorio para teste

---
+++ gerarGrafoRandom.py
@@ -0,0 +1,18 @@
+import random
+
+num_vertices = 3000  # Número de vértices desejado#
+num_arestas = 20000  # Número de arestas desejado
+
+# Gerar arestas aleatórias
+arestas = set()
+while len(arestas) < num_arestas:
+    origem = random.randint(1, num_vertices)
+    destino = random.randint(1, num_vertices)
+    if origem != destino:
+        arestas.add((origem, destino))
+
+# Escrever o grafo no formato do arquivo grafo.txt
+with open("grafo_500.txt", "w") as f:
+    f.write(f"{num_vertices} 0 0 0\n")
+    for origem, destino in arestas:
+        f.write(f"{origem} {destino}\n")
